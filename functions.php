90ded9e0476cdbf2079f056744339e16e0e283bd
Paul <paulgsepulveda@gmail.com>
2021-10-15 10:24:37 -0400

    renamed placeholders

--- functions.php
+++ functions.php
@@ -1,11 +1,11 @@
 <?php
-namespace MyProject\Theme;
+namespace STEM\Theme;
 
-define( 'MYPROJECT_THEME_DIR', trailingslashit( get_stylesheet_directory() ) );
+define( 'STEM_THEME_DIR', trailingslashit( get_stylesheet_directory() ) );
 
 
 // Theme foundation
-include_once MYPROJECT_THEME_DIR . 'includes/config.php';
-include_once MYPROJECT_THEME_DIR . 'includes/meta.php';
+include_once STEM_THEME_DIR . 'includes/config.php';
+include_once STEM_THEME_DIR . 'includes/meta.php';
 
 // Add other includes to this file as needed.
--- includes/config.php
+++ includes/config.php
@@ -2,11 +2,11 @@
 /**
  * Handle all theme configuration here
  **/
-namespace MyProject\Theme\Includes\Config;
+namespace STEM\Theme\Includes\Config;
 
 
-define( 'MYPROJECT_THEME_URL', get_stylesheet_directory_uri() );
-define( 'MYPROJECT_THEME_STATIC_URL', MYPROJECT_THEME_URL . '/static' );
-define( 'MYPROJECT_THEME_CSS_URL', MYPROJECT_THEME_STATIC_URL . '/css' );
-define( 'MYPROJECT_THEME_JS_URL', MYPROJECT_THEME_STATIC_URL . '/js' );
-define( 'MYPROJECT_THEME_IMG_URL', MYPROJECT_THEME_STATIC_URL . '/img' );
+define( 'STEM_THEME_URL', get_stylesheet_directory_uri() );
+define( 'STEM_THEME_STATIC_URL', STEM_THEME_URL . '/static' );
+define( 'STEM_THEME_CSS_URL', STEM_THEME_STATIC_URL . '/css' );
+define( 'STEM_THEME_JS_URL', STEM_THEME_STATIC_URL . '/js' );
+define( 'STEM_THEME_IMG_URL', STEM_THEME_STATIC_URL . '/img' );
